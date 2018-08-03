2d5e4284051f01952871c86dd3bb96e134eb52db
Stephen Chiang <skhchiang@gmail.com>
2018-08-03 17:34:44 +0800

    attempt

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path')
 const app = new express();
 
+app.use(express.static(path.join(__dirname + "/assets")));
+
 app.get('/', function(req, res){
-res.sendFile('./index.html')
+res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(8080);
