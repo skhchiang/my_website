ff4fb057830412aba9f3f3c311d32eac1e49cd38
Stephen Chiang <skhchiang@gmail.com>
2018-10-08 11:59:26 +0800

    console logged port

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.get('/', function(req, res){
 res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.listen(8080);
+app.listen(8080,() => {
+    console.log(`Application started at port: 8080`)});
 
 
 
